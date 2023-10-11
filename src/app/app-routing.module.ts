fc752e18e17e56875ff838a5d59100901e1be3e0
Abraham Cordero <abrahamicm2@gmail.com>
2023-10-11 10:49:11 -0400

    '.'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,19 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'categorias',
+    loadChildren: () => import('./pages/categorias/categorias.module').then( m => m.CategoriasPageModule)
+  },
+  {
+    path: 'notas',
+    loadChildren: () => import('./pages/notas/notas.module').then( m => m.NotasPageModule)
+  },
+  {
+    path: 'preferencias',
+    loadChildren: () => import('./pages/preferencias/preferencias.module').then( m => m.PreferenciasPageModule)
   }
+
 ];
 @NgModule({
   imports: [
